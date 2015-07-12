7f52ab381fa4372508f302e3cb1cb3c024f3d7ba
Simon Persson <simon@flaskpost.org>
2015-07-12 15:35:52 +0000

    Update example.

--- examples/example.rs
+++ examples/example.rs
@@ -2,15 +2,11 @@
 extern crate ease;
 
 use ease::Client;
-use ease::UserAgent;
 
 fn main() {
     println!("{}", 
-             Client::new("https://api.discogs.com/releases/249504")
+             Client::new("http://httpbin.org/get")
                         .ok().expect("Couldn't parse URL.")
-                        .header(
-                            UserAgent("FooBarApp/2.0".to_owned())
-                            )
                         .get()
                         .unwrap()
             );
@@ -22,6 +18,22 @@ fn main() {
                         .ok().expect("Couldn't parse URL.")
                         .param(("foo", "bar"))
                         .post()
+                        .unwrap()
+            );
+
+    println!("{}", 
+             Client::new("http://httpbin.org/put")
+                        .ok().expect("Couldn't parse URL.")
+                        .param(("foo", "bar"))
+                        .put()
+                        .unwrap()
+            );
+
+    println!("{}", 
+             Client::new("http://httpbin.org/delete")
+                        .ok().expect("Couldn't parse URL.")
+                        .param(("foo", "bar"))
+                        .delete()
                         .unwrap()
             );
 }
