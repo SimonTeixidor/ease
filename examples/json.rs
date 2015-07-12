d632fbb691ffe7a6d95f8ace5929df13bf2ad4e6
Simon Persson <simon@flaskpost.org>
2015-07-12 18:31:43 +0000

    Add JSON and POST example.

--- examples/json.rs
+++ examples/json.rs
@@ -10,16 +10,25 @@ use ease::RestClient;
 #[derive(Deserialize, Debug)]
 struct Response {
     args: HashMap<String, String>,
+    data: Option<String>,
+    files: HashMap<String, String>,
+    form: HashMap<String, String>,
     headers: HashMap<String, String>,
+    json: Option<String>,
     origin: String,
     url: String
 }
 
 fn main() {
     println!("{:?}",
+             RestClient::new("http://httpbin.org/post")
+                        .ok().expect("Couldn't parse URL.")
+                        .post_json_as::<Response>()
+        );
+    
+    println!("{:?}",
              RestClient::new("http://httpbin.org/get")
                         .ok().expect("Couldn't parse URL.")
                         .get_json_as::<Response>()
-                        .unwrap()
         );
 }
