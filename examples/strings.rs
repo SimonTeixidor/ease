b094979234ee2a5f8091e6544d1a784dbe3aae08
bluss <bluss@users.noreply.github.com>
2015-09-12 17:13:43 +0200

    Elaborate a request example to use non-'static parameters

--- examples/strings.rs
+++ examples/strings.rs
@@ -3,6 +3,15 @@ extern crate ease;
 use ease::{Url, Request};
 
 fn main() {
+    // simple
     let url = Url::parse("http://httpbin.org/get").unwrap();
     println!("{}", Request::new(url).param("foo", "bar").get().unwrap().body);
+
+    // multiple
+    let url = Url::parse("http://httpbin.org/get").unwrap();
+    let value = "1729".to_owned();
+    let mut req = Request::new(url);
+    req.param("foo", &value);
+    req.param("bar", &value);
+    println!("{}", req.get().unwrap().body);
 }
